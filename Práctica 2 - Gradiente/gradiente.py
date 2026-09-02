5be44b496cf2778cc8f94a301d54603f57767eb7
dezzAndy <diego.quevedo5103@alumnos.udg.mx>
2026-09-02 02:48:56 -0600

    Práctica 2 terminada

--- Práctica 2 - Gradiente/gradiente.py
+++ Práctica 2 - Gradiente/gradiente.py
@@ -1,33 +1,63 @@
+"""
+Gradiente Descendente para Rosenbrock
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def Gradiente(x, y, LR = 0.01, iter = 10000):
+def rosenbrock(x, y):
+    """
+    Función de Rosenbrock
+    """
+    return (1 - x) ** 2 + 100 * (y - x ** 2) ** 2
+
+def gradiente_rosenbrock(x: float, y: float, lr=0.001, iteraciones=10000):
+    """
+    Gradiente Descendente para Rosenbrock
+    """
+    historial_costo = []
+
+    for _ in range(iteraciones):
+        costo = rosenbrock(x, y)
+        historial_costo.append(costo)
+        # Derivadas parciales de la función de Rosenbrock
+        dx = -2 * (1 - x) - 400 * x * (y - x ** 2)
+        dy = 200 * (y - x ** 2)
+        # Actualización de pesos
+        x = x - lr * dx
+        y = y - lr * dy
+
+    return x, y, historial_costo
+
+def gradiente(x: float, y: float, lr = 0.01, iter: int = 10000):
+    """
+    Gradiente Descendente para regresión lineal
+    """
     n = len(y)
     m = 0.0
     b = 0.0
     historial_costo = []
 
-    for i in range(iter):
+    for _ in range(iter):
         # función objetivo
-        y_pred = m * x * 8 * np.sin(x) + b
-        # y_pred = m * x + b
-        costo = 1 / n * np.sum(x * (y - y_pred) ** 2) # Calculo error cuadrático medio
+        y_pred = m * x + b
+        costo = 1 / n * np.sum((y - y_pred) ** 2) # Calculo error cuadrático medio
         historial_costo.append(costo)
         dm = (-2 / n) * np.sum(x * (y - y_pred)) # Derivadas
         db = (-2 / n) * np.sum(y - y_pred)
-        m = m - LR * dm
-        b = b = LR * db
+        m = m - lr * dm
+        b = b - lr * db
     return m, b, historial_costo
 
 np.random.seed(42)
-X = np.array([1, 2, 3, 4, 5], dtype=float)
-Y = np.array([5, 7, 9, 11, 13], dtype=float)
+# Inicialización de datos para el posterior gradiente descendente
+X_INICIAL = -1.0
+Y_INICIAL = 2.0
 
-m_opt, b_opt, costo_hist = Gradiente(X, Y, LR=0.000005, iter=500)
+x_opt, y_opt, costo_hist = gradiente_rosenbrock(X_INICIAL, Y_INICIAL, lr=0.000005, iteraciones=500)
 
-print(f"Pendiente óptimo (m): {m_opt:.4f}")
-print(f"Intersección óptima (b): {b_opt:.4f}")
-print(f"Costo final: {costo_hist[-1]:.6f}")
+print(f"X óptimo: {x_opt:.4f}") # Debería acercarse a 1.0
+print(f"Y óptima: {y_opt:.4f}") # Debería acercarse a 1.0
+print(f"Costo final: {costo_hist[-1]:.6f}") # Debería acercarse a 0.0
 
 plt.plot(costo_hist, marker="o", markersize=3)
 plt.xlabel("Iteración")
@@ -35,6 +65,3 @@ plt.ylabel("Mejor Fitness Encontrado")
 plt.title("Gradiente")
 plt.grid(True)
 plt.show()
-
-# agregar grafico //
-# cambiar linea 10
